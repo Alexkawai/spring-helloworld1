d1c9091cb058fbac2ea86f415427420954ffc1e1
nikitanesterenkos@gmail.com <piramida741147>
2020-07-03 12:55:27 +0300

    fix log

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld tree =
+                applicationContext.getBean(HelloWorld.class);
+        HelloWorld leaf =
+                applicationContext.getBean(HelloWorld.class);
+
+        Cat one = applicationContext.getBean(Cat.class);
+        Cat two = applicationContext.getBean(Cat.class);
+        System.out.println(tree==leaf);
+        System.out.println(one==two);
+
     }
 }
